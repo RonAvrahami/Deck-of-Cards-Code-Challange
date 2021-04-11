a31c2ae052263cd7626644c6fed64d37cebe169b
Ron Avrahami <118avrahami@gmail.com>
2021-04-11 12:57:43 -0600

    Set up

---
+++ CodeChallange2/CodeChallange2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallange2
+//
+//  Created by Ron Avrahami on 4/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("")
+    }
+
+
+}
+
