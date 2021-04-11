d63287f84ea4adf08ecacc9d5588cf51fbf2f723
Ron Avrahami <118avrahami@gmail.com>
2021-04-11 13:32:28 -0600

    CollectionView

---
+++ CodeChallange2/CodeChallange2/JSONManager.swift
@@ -0,0 +1,24 @@
+//
+//  JSONManager.swift
+//  CodeChallange2
+//
+//  Created by Ron Avrahami on 4/11/21.
+//
+
+import Foundation
+
+class JSONManager {
+    
+    func getCards() {
+        // https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+
+    }
+    
+    func drawCards() {
+        // https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=5
+
+    }
+    
+    
+    
+}
