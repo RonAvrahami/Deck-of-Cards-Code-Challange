c46e5bf40ed6b1a748c9525dda831a38635d424d
Ron Avrahami <118avrahami@gmail.com>
2021-04-11 18:08:04 -0600

    Working Animation

--- CodeChallange2/CodeChallange2/ViewController.swift
+++ CodeChallange2/CodeChallange2/ViewController.swift
@@ -14,6 +14,7 @@ enum Section {
 class ViewController: UIViewController, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var redrawButton: UIButton!
     
     var dataSource: CardDataSource!
     var sections = [Section]()
@@ -25,7 +26,7 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         
         collectionView.delegate = self
         collectionView.allowsSelection = false
-        //collectionView.isScrollEnabled = false
+        collectionView.isScrollEnabled = false
         
         collectionView.collectionViewLayout = configureCollectionViewLayout()
         
@@ -43,7 +44,6 @@ class ViewController: UIViewController, UICollectionViewDelegate {
             if let image = card.uiImage {
                 cell?.update(image: image)
             }
-            
             return cell
         })
         updateDataSource()
@@ -58,41 +58,43 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         
         sections = snapshot.sectionIdentifiers
         dataSource.apply(snapshot, animatingDifferences: true, completion: nil)
+        
     }
     
     func configureCollectionViewLayout() -> UICollectionViewLayout {
-
+        
         let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-
-            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(100), heightDimension: .absolute(200))
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
+            
             let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 5)
             group.interItemSpacing = .fixed(-100)
+            
             let section = NSCollectionLayoutSection(group: group)
-            //section.interGroupSpacing = -100
-
+            
             return section
         }
         return layout
     }
     
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-        let myrect = cell.frame
-        cell.frame = CGRect(x: cell.frame.origin.x+320, y: cell.frame.origin.y, width: cell.frame.size.width, height: cell.frame.size.height)
-        
-        let value = Double(indexPath.row)*0.1
-        UIView.animate(withDuration: 0.8, delay:value, options: .curveEaseInOut, animations: {
-            
-            cell.frame = CGRect(x: myrect.origin.x+100, y: myrect.origin.y, width: myrect.size.width, height: myrect.size.height)
-            
-        }) { (finish) in
-            
-            UIView.animate(withDuration: 0.8, animations: {
-                cell.frame = myrect
-            })
+        cell.isHidden = true
+        self.redrawButton.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            cell.isHidden = false
+            let myreact = cell.frame
+                    cell.frame = CGRect(x: cell.frame.origin.x+320, y: cell.frame.origin.y, width: cell.frame.size.width, height: cell.frame.size.height)
+
+                     let value = Double(indexPath.row)*0.1
+                     UIView.animate(withDuration: 0.8, delay:value, options: .curveEaseInOut, animations: {
+
+                        cell.frame = CGRect(x: myreact.origin.x, y: myreact.origin.y, width: myreact.size.width, height: myreact.size.height)
+
+                     }) { (_) in
+                self.redrawButton.isEnabled = true
+            }
         }
     }
     
